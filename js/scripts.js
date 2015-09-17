df64b7ebd0c9f376429973c6646405bb4f48942a
Rick Hills & christopher calascione <student@epicodus.com>
2015-09-17 14:45:09 -0700

    Add Play Computer in Random Pick mode.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
-var player, mark, turns, newBoard;
+var player, computer, mark, turns, spaceNumber, gameOver, newBoard;
 
 $(document).ready(function() {
     $("button").click(function() {
+        // read value for 2-player or computer and assign. 4 possible values read from page
+        $("button").addClass("hide");
+        gameOver = 0;
+        computer = $("input:radio[name='game-type']:checked").val();
+        console.log(computer);
         player = 1;
         turns = 1;
         newBoard = new Board([0,0,0,0,0,0,0,0,0]);
         $(".space").empty();
         $(".player-turn").text(getPlayer());
 
+
         $(".space").one('click', function() {
+            // debugger;
             mark = getMark();
             turn($(this).attr("value"));
             $(this).append("<h1 class='"+mark+"'> "+mark+" </h1>");
-            testWin(newBoard.board);
+            if ( !gameOver ) { testWin(newBoard.board); }
             $(".player-turn").text(getPlayer());
+            if( computer > 0 && !gameOver ) {
+                mark = getMark();
+                spaceNumber = randomPick();
+                $(".space[value="+spaceNumber+"]").append("<h1 class='"+mark+"'> "+mark+" </h1>");
+                $(".player-turn").text(getPlayer());
+                testWin(newBoard.board)
+            }
         });
     });
 });
@@ -46,19 +60,30 @@ function testWin(board) {
         diag2 = board[0] + board[4] + board[8];
 
     if(row1 === 3 || row2 ===3 || row3 ===3 || col1 ===3 || col2 ===3 || col3 ===3 || diag1 ===3 || diag2 ===3) {
+        gameOver = 1;
         $(".space").off("click");
-        return alert("Player 1 Wins!");
+        alert("Player 1 Wins!");
+        location.reload();
     } else if (row1 === 12 || row2 ===12 || row3 ===12 || col1 ===12 || col2 ===12 || col3 ===12 || diag1 ===12 || diag2 ===12) {
+        gameOver = 1;
         $(".space").off("click");
-        return alert("Player 2 Wins!");
-    } else if (turns === 10) { return alert("Cat's Game"); }
+        alert("Player 2 Wins!");
+        location.reload();
+    } else if (turns === 10) {
+        alert("Cat's Game");
+        location.reload();
+    }
 }
 
 function turn(index){
+        // debugger;
         if( newBoard.markBoard(player, index) ) {
             player = player ? 0:1;
             turns++;
-        } else { alert("That number has been taken already!")}  // if playing computer on random mode else generates random #
+        } else {
+            // Logic for computer or second player
+            alert("That number has been taken already!");
+        }  // if playing computer on random mode else generates random #
 }
 
 function getPlayer() {
@@ -71,6 +96,16 @@ function getMark() {
     return temp;
 }
 
-var random = function() {
-    return randomNumber = Math.floor((Math.random() * 7 + 1));
+function randomPick() {
+    do {
+         var res = Math.random();
+         res = res * 9;
+         randomNumber = Math.floor(res);
+    } while ( newBoard.board[randomNumber] > 0 );
+
+    newBoard.markBoard(player, randomNumber);
+    player = player ? 0:1;
+    turns++;
+
+    return randomNumber;
 }
